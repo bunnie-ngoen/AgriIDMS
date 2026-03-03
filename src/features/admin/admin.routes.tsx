1d08e4d43ffeb82bd196c57d3b7b9ea7c89ead15
namvu <namvphe173235@fpt.edu.vn>
2026-03-04 01:15:34 +0700

    Update auth + admin features

--- src/features/admin/admin.routes.tsx
+++ src/features/admin/admin.routes.tsx
@@ -2,17 +2,25 @@
 import { lazy } from "react";
 
 const DashboardPage = lazy(() => import("../admin/pages/AdminDashboard"));
-const AdminLayout = lazy(() => import("../admin/components/layouts/AdminLayout"));
-const CreateUserPage = lazy(() => import("../admin/pages/CreateUser"));
+const AdminLayout = lazy(
+  () => import("../admin/components/layouts/AdminLayout")
+);
+const CreateUserPage = lazy(
+  () => import("../admin/pages/CreateUser")
+);
+const UserListPage = lazy(
+  () => import("../admin/pages/UserList")
+);
 
 export const adminRoutes = [
   {
     path: "/admin",
     element: <AdminLayout />,
     children: [
-      { index: true, element: <DashboardPage /> }, // Thêm lại dòng này
-      { path: "dashboard", element: <DashboardPage /> }, // Thêm lại dòng này
-      { path: "create-user", element: <CreateUserPage/> },
+      { index: true, element: <DashboardPage /> },
+      { path: "dashboard", element: <DashboardPage /> },
+      { path: "create-user", element: <CreateUserPage /> },
+      { path: "users", element: <UserListPage /> },
     ],
   },
 ];
